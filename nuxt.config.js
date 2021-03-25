1fd8de8dc67ad941bb39c018a84fa76f200f4a30
Zain Ur Rehman <zainkhan.official10@gmail.com>
2021-03-25 18:00:58 +0500

    rest api ip changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -83,7 +83,7 @@ export default {
    */
   axios: {
     // baseURL: 'http://139.59.73.151:3000'
-    baseURL: 'http://165.232.184.249:3000'
+    baseURL: 'http://localhsot:3000'
     // baseURL: 'https://api.acpshopping.com'
   },
   /*
